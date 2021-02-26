1ce1d0f7038a3c055554326576854528933da30c
Shashi Kiran <shashikiran@Shashis-MacBook-Pro.local>
2021-02-26 22:58:37 +0530

    V0.0.1

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 const body = document.querySelector("body");
-const centered = document.querySelector('.c-centered')
+const centered = document.querySelector('.c-centered');
+var styleEl = document.createElement('style');
 class Text {
     constructor(text) {
         this.text = text;
-        const txt = document.createElement('p');
-        txt.innerHTML = this.text;
-        txt.classList.add('c-centered');
-        txt.style.color = "#007bff";
-        return txt
+        document.write(`<p class="kode-text">${this.text}</p>`)
+        this.value = document.querySelectorAll('.kode-text');
+        // console.log(this.value)
+        this.value.innerHTML = this.text;
+        this.center1();
+        return this.value;
     }
     font() {
         const hex = arguments[0];
         const style = arguments[1];
         const decor = arguments[2];
     }
-    txt() {
-        const txt = document.createElement('p');
-        txt.innerHTML = this.text;
-        txt.classList.add('c-centered');
-        txt.style.color = "#007bff";
-        return txt;
-    }
-    center() {
-        var styleEl = document.createElement('style');
-        styleEl.innerHTML = 'c-centered{color:blue; !important}';
-        document.head.appendChild(styleEl);
+    // txt() {
+    //     const value = document.querySelector('.text');
+    //     value.classList.add('c-centered')
+    //     var styleEl = document.createElement('style');
+    //     styleEl.innerHTML = 'c-centered{color:blue; !important}';
+    //     document.head.appendChild(styleEl);
+    //     return this.text
+    // }
+    center1() {
+        const kode_text = document.querySelector('.kode-text');
+        console.log(kode_text)
+        kode_text.setAttribute("style", "color: #007bff;");
     }
 }
 
 const text = new Text("Test");
-body.innerHTML = text.center();
+// text.center()
+kode.innerHTML = `hello, ${text.innerHTML}`;
